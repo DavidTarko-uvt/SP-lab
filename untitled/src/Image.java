c5e3d56161a3616265f9de61c43f3680b2330de2
DavidTarko-uvt <david.tarko01@e-uvt.ro>
2022-11-19 15:43:59 +0200

    no message

--- untitled/src/Image.java
+++ untitled/src/Image.java
@@ -1,8 +1,23 @@
-public class Image {
-    public String Imagename;
+public class Image extends Element{
+    public String URL;
     public Image(String s) {
-        this.Imagename=s;
+        this.URL=s;
     }
+
+    @Override
+    public void add(Element element) {
+    }
+
+    @Override
+    public void get(int id) {
+    }
+
+    @Override
+    public void remove(Element element) {
+    }
+
     public void print()
-    {}
+    {
+        System.out.println("Image with Image name:"+URL);
+    }
 }
